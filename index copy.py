dc177a391ce55b9c7871ee22bfd6935d578f22d2
Jean Patimeteeporn <jpatimet@gmail.com>
2024-02-22 09:09:15 -0600

    added feed method and string method

---
+++ index copy.py
@@ -0,0 +1,46 @@
+# import the python datetime module to help us create a timestamp
+from datetime import date
+
+
+miss_fuzz = Llama("Miss Fuzz", "domestic llama")
+
+
+yogi = Bear("Yogi", "black bear")
+
+
+eeyore = Donkey("Eeyore", "donkey")
+
+gabby = Goat("Gabby", "goat")
+
+
+hunter = Catfish("Hunter", "cat fish")
+
+
+kale = Copperhead("Kale", "copperhead snake")
+
+
+steve = Snail("Steve", "snail")
+
+
+franklin = Turtle("Franklin", "turtle")
+
+
+bojack = Horse("Bojack", "horse")
+
+
+marty = ClownFish("Marty", "clown fish")
+
+
+daffy = Mallard("Daffy", "mallard")
+
+
+fudge = Goldfish("Fudge", "gold fish")
+
+
+randall = RatSnake("Randall", "rat snake")
+
+
+patrick = Python("Patrick", "python")
+
+
+kobe = BlackMamba("Kobe", "black mamba")
